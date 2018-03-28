306b04b6caf3470240e5b204759f508b25fbe1af
Shedlia Freeman <sfreeman@basecampcodingacademy.org>
2018-03-27 20:32:18 -0500

    The account summary page
    this only returns if information was gathered or not right now

--- src/AccountSummary.js
+++ src/AccountSummary.js
@@ -1,43 +1,6 @@
 import React, { Component } from "react";
 import { bake_cookie, read_cookie, delete_cookie } from "sfcookies";
 
-var dummyUser = {
-  id: 1999,
-  username: "shedliaf",
-  firstName: "Shedlia",
-  LastName: "Freeman",
-  balance: 20,
-  transactions: [
-    // id
-    // day
-    // what was given or received
-    // to or from the company
-    {
-      id: 1,
-      day: "11-2-2017",
-      amount: 5,
-      from: "Jo"
-    },
-    {
-      id: 2,
-      day: "11-5-2017",
-      amount: 5,
-      from: "Bill"
-    },
-    {
-      id: 3,
-      day: "11-25-2017",
-      amount: 15,
-      from: "Kell"
-    },
-    {
-      id: 4,
-      day: "12-2-2017",
-      amount: -5,
-      from: "Jo"
-    }
-  ]
-};
 function Transactions(props) {
   const transactions = props.transactions.map(transaction => (
     <li key={transaction.id.toString()}> Amount: {transaction.amount} </li>
@@ -49,51 +12,16 @@ class AccountSummary extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userInfo: {}
+      userInfo: {},
+      loaded: false
     };
-  }
-  componentDidMount() {
-    fetch("http://localhost:8080/login", {
-      method: "post",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        Username: "shedliafreeman",
-        FirstName: "Shedlia",
-        LastName: "Freeman",
-        Password: "1234",
-        Email: "shedliaf@gmail.com"
-      })
-    })
-      .then(function(response) {
-        return response.json();
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
-      .then(function(key) {
-        bake_cookie("CUser", key);
-      });
   }
 
   render() {
     if (!this.state.loaded) {
-      return <div> not loaded </div>;
+      return <div className="alert alert-danger"> key not loaded </div>;
     } else {
-      return (
-        <div className="jumbotron">
-          <h1 className="display-4">Hello, {this.state.userInfo.username}!</h1>
-          <p className="lead">
-            Your name:{" "}
-            {this.state.userInfo.firstName + this.state.userInfo.LastName}
-            id: {this.state.userInfo.id}
-          </p>
-          <p />
-          <Transactions transactions={this.state.userInfo.transactions} />
-        </div>
-      );
+      return <div className="alert alert-success"> It was loaded </div>;
     }
   }
 }
