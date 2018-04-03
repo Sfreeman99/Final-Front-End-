5f7c678585e71ea5e689fd057e63b4eb972a42fa
Shedlia Freeman <sfreeman@basecampcodingacademy.org>
2018-04-03 01:15:09 -0500

    added a private route that show certain components only if you are logged in

--- src/App.js
+++ src/App.js
@@ -7,26 +7,35 @@ import { Route, Redirect, BrowserRouter as Router } from "react-router-dom";
 import { bake_cookie, read_cookie, delete_cookie } from "sfcookies";
 import "./App.css";
 import { getToken } from "./CookieInformation";
-
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      getToken() === true ? <Component {...props} /> : <Redirect to="/signup" />
+    }
+  />
+);
+// const LoggedIn = ({ component: Component, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={props =>
+//       getToken() === true ? <Component {...props} /> : <Redirect to="/accountSummary" />
+//     }
+//   />
+// );
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
           <Route
-            exact
             path="/"
-            render={() => {
-              return getToken() === true ? (
-                <Redirect to="/accountSummary" />
-              ) : (
-                // <Redirect to="/login" />
-                <Redirect to="/signup" />
-              );
+            component={() => {
+              return <Redirect to="/signup" />;
             }}
           />
           <Route path="/login" component={Login} />
-          <Route path="/accountSummary" component={AccountSummary} />
+          <PrivateRoute path="/accountSummary" component={AccountSummary} />
           <Route path="/signup" component={Signup} />
         </div>
       </Router>
