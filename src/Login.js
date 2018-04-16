8561e025fba5ef145a5519562e80117ceda9fc35
Shedlia Freeman <sfreeman@basecampcodingacademy.org>
2018-04-16 16:22:20 -0500

    Set up CSS configuration for the Login Form

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { bake_cookie, read_cookie } from "sfcookies";
 import { getToken } from "./CookieInformation";
 import { Redirect, Link } from "react-router-dom";
+import { Navbar } from "./Signup";
+import "./Login.css";
 
 class Login extends Component {
   constructor(props) {
@@ -82,72 +84,87 @@ class Login extends Component {
       return <Redirect to="/accountSummary" />;
     }
     return (
-      <form
-        className="needs-validation login-form-margin-top"
-        onSubmit={this.handleSubmit}
-      >
-        <div className="form-group col-lg-4 offset-lg-4">
-          <label htmlFor="Username" className="col-lg-2 col-form-label">
-            Username
-          </label>
-          <div className="col-lg-12">
-            <input
-              type="text"
-              className={
-                "form-control input-form " +
-                (this.state.error ? "is-invalid" : "")
-              }
-              id="Username"
-              placeholder="Username"
-              name="username"
-              value={this.state.username}
-              onChange={event =>
-                this.setState({
-                  username: event.target.value,
-                  error: false
-                })
-              }
-            />
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="offset-lg-3 col-lg-6">
+            <div className="card login">
+              <div className="card-header">
+                {" "}
+                <h3 className="login">Login </h3>{" "}
+              </div>
+              <div className="card-body">
+                <form
+                  className="needs-validation login-form-margin-top"
+                  onSubmit={this.handleSubmit}
+                >
+                  <div className="form-group">
+                    <div className="col-lg-12">
+                      <input
+                        type="text"
+                        className={
+                          "form-control input-form " +
+                          (this.state.error ? "is-invalid" : "")
+                        }
+                        id="Username"
+                        placeholder="Username"
+                        name="username"
+                        value={this.state.username}
+                        onChange={event =>
+                          this.setState({
+                            username: event.target.value,
+                            error: false
+                          })
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-lg-12">
+                      <input
+                        type="password"
+                        className={
+                          "form-control input-form " +
+                          (this.state.error ? "is-invalid" : "")
+                        }
+                        id="Password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        name="password"
+                        onChange={event =>
+                          this.setState({
+                            password: event.target.value,
+                            error: false
+                          })
+                        }
+                      />
+                      <div className="invalid-feedback">
+                        Username/Password is invalid
+                      </div>
+                    </div>
+                  </div>
+                  <div className="button-position">
+                    <Link
+                      id="back-button"
+                      className="btn btn-default btn-lg"
+                      to="/signup"
+                    >
+                      Back
+                    </Link>
+                    <button
+                      id="login"
+                      className="btn btn-primary btn-lg center"
+                      type="submit"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="form-group col-lg-4 offset-lg-4">
-          <label htmlFor="Password" className="col-lg-2 col-form-label">
-            Password
-          </label>
-          <div className="col-lg-12">
-            <input
-              type="password"
-              className={
-                "form-control input-form " +
-                (this.state.error ? "is-invalid" : "")
-              }
-              id="Password"
-              placeholder="Password"
-              value={this.state.password}
-              name="password"
-              onChange={event =>
-                this.setState({
-                  password: event.target.value,
-                  error: false
-                })
-              }
-            />
-            <div className="invalid-feedback">Username/Password is invalid</div>
-          </div>
-        </div>
-        <div className="col-lg-4 offset-lg-4 signup-submit-button-width">
-          <Link className="btn btn-danger btn-lg" to="/signup">
-            Back
-          </Link>
-          <button
-            id="login"
-            className="btn btn-primary btn-lg center"
-            type="submit"
-          >
-            Login
-          </button>
-        </div>
-      </form>
+      </div>
     );
   }
 }
